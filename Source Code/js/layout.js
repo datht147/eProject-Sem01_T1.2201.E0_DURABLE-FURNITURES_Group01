8a4282e6fe7ac98628758294f499cdc9575ac039
Dat Huynh <99384449+datht147@users.noreply.github.com>
2022-05-21 14:07:24 +0700

    Delete Source Code directory

--- Source Code/js/layout.js
+++
@@ -1,9 +0,0 @@
-
-$(function(){
-    // $("#filterProducts").load("filter.html");
-
-    $("#includedNav").load("header.html"); 
-    $("#includedFooter").load("footer.html"); 
-  });
-  console.log("layout.js loaded");
-  
